c45d51a17cb37019127709eae23ee9af875c646e
AUTBLT410 <AUTBLT410@LT410-50.allianceblr.com>
2024-10-30 15:45:16 +0530

    this is my github push

---
+++ src/abc/firstpro.java
@@ -0,0 +1,11 @@
+package abc;
+
+public class firstpro {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to Alliance - jenkins");
+		// TODO Auto-generated method stub
+
+	}
+
+}
